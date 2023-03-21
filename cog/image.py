aa1a4e9d6441d638b94254f1b5f21ae0dcb94064
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-03-21 23:16:08 +0800

    added options by user id

--- cog/image.py
+++ cog/image.py
@@ -2,22 +2,39 @@ import base64
 import io
 import os
 import random
+import copy
+import asyncio
 
 import discord
 import requests
 from discord.ext import commands
 from PIL import Image
 
-from config import img_config
+from config import config, img_config
 
 aliases = {
     'image': ['img'],
-    'sd_model': [],
-    'sd': ['stable_diffusion', 'gen'],
+    'sd_model': ['model'],
+    'sd': ['stable_diffusion', 'diffuse'],
+    'sd_options': ['so'],
+    'sd_print_options': ['spo'],
 }
 
-class ImageCog(commands.Cog, name='Image Generator'):
-    @commands.command('image', aliases=aliases['image'], help='Generates image')
+class ImageCog(commands.Cog, name='Image Generator' if not config.be_funny else '不可以色色'):
+    def __init__(self):
+        super()
+        self.cfgs = {}
+        self.lock = asyncio.Lock()
+
+    async def get_payload(self, ctx: commands.Context):
+        if ctx.author.id in self.cfgs.keys():
+            return self.cfgs[ctx.author.id]
+        
+        self.cfgs[ctx.author.id] = copy.deepcopy(img_config.txt2img_payload)
+
+        return self.cfgs[ctx.author.id]
+
+    @commands.command('image', aliases=aliases['image'], help='Chooses random image from directory')
     async def image_(self, ctx, *args):
         arg = ' '.join(args)
         img_dir = img_config.img_dir
@@ -27,47 +44,82 @@ class ImageCog(commands.Cog, name='Image Generator'):
         await ctx.send(file=discord.File(os.path.join(img_dir, arg, img_file)))
 
     @commands.command('sd_model', aliases=aliases['sd_model'], help='Set stable diffusion model, default = chilloutmix')
-    async def model_(self, ctx, model='chilloutmix'):
-        payload = img_config.options_payload
+    async def sd_model_(self, ctx, model=None):
+        payload = await self.get_payload(ctx)
 
         if model not in img_config.model_map.keys():
-            embed = discord.Embed(title='', description='Model not installed', color=discord.Color.dark_red())
+            embed = discord.Embed(title='Model not installed', description=f'SFW models {img_config.sfw_models}\nNSFW models {img_config.nsfw_models}', color=discord.Color.dark_red())
             return await ctx.send(embed=embed)
 
         payload['sd_model_checkpoint'] = img_config.model_map[model]
-
-        response = requests.post(url=f'{img_config.sd_url}/sdapi/v1/options', json=payload)
-
-        if response.status_code != 200:
-            embed = discord.Embed(title='', description='Failed to load model', color=discord.Color.dark_red())
-            return await ctx.send(embed=embed)
         
-        embed = discord.Embed(title='', description=f'Successfully loaded model {payload["sd_model_checkpoint"]}', color=discord.Color.green())
-        return await ctx.send(embed=embed)
+        embed = discord.Embed(title='', description=f'Successfully loaded {payload["sd_model_checkpoint"]}', color=discord.Color.green())
+        await ctx.send(embed=embed)
 
     @commands.command('sd', aliases=aliases['sd'], help='Generates stable diffusion image, enter postive and negative prompt, e.g. \sd "corgi" "bad anatomy, watermark"')
     async def sd_(self, ctx, pos_prompt, neg_prompt=''):
         sd_path = os.path.join(img_config.img_dir, 'sd')
         os.makedirs(sd_path, exist_ok=True)
 
-        payload = img_config.txt2image_payload
+        payload = await self.get_payload(ctx)
         payload['prompt'] = pos_prompt
         payload['negative_prompt'] = neg_prompt
+        options = {'sd_model_checkpoint': payload['sd_model_checkpoint']}
 
-        response = requests.post(url=f'{img_config.sd_url}/sdapi/v1/txt2img', json=payload)
-        r = response.json()
+        async with self.lock:
+            response = requests.post(url=f'{img_config.sd_url}/sdapi/v1/options', json=options)
+            if response.status_code != 200:
+                embed = discord.Embed(title='', description='Failed to load model', color=discord.Color.dark_red())
+                return await ctx.send(embed=embed)
+
+            response = requests.post(url=f'{img_config.sd_url}/sdapi/v1/txt2img', json=payload)
 
         if response.status_code != 200:
-            embed = discord.Embed(title='', description='Failed to generate iamge', color=discord.Color.dark_red())
+            embed = discord.Embed(title='', description='Failed to generate image', color=discord.Color.dark_red())
             return await ctx.send(embed=embed)
         
-        for i in r['images']:
+        num = sorted(map(lambda x: int(os.path.splitext(x)[0]), os.listdir(sd_path)))[-1]
+        for i in response.json()['images']:
+            num += 1
+
             image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+            image.save(os.path.join(sd_path, f'{num}.jpg'))
 
-            num_images = len(os.listdir(sd_path))
-            image.save(os.path.join(sd_path, f'{num_images}.jpg'))
+            embed = discord.Embed(title='', description=f'{payload["sd_model_checkpoint"]} generated', color=discord.Color.green())
+            await ctx.send(embed=embed)
+            await ctx.send(file=discord.File(os.path.join(sd_path, f'{num}.jpg')))
 
-            await ctx.send(file=discord.File(os.path.join(sd_path, f'{num_images}.jpg')))
+    @commands.command('sd_options', aliases=aliases['sd_options'], help='Set options')
+    async def sd_options_(self, ctx, key=None, value=None):
+        if key not in img_config.valid_options.keys():
+            embed = discord.Embed(title='', description=f'Please enter valid options {list(img_config.valid_options.keys())}', color=discord.Color.dark_red())
+            return await ctx.send(embed=embed)
+        
+        if key == 'cfg_scale':
+            try:
+                value = float(value)
+            except:
+                pass
+        elif key in ['steps', 'height', 'width']:
+            try:
+                value = int(value)
+            except:
+                pass           
+
+        if value not in img_config.valid_options[key]:
+            embed = discord.Embed(title='', description=f'Please enter valid value {img_config.valid_options[key]}', color=discord.Color.dark_red())
+            return await ctx.send(embed=embed)
+
+        payload = await self.get_payload(ctx)
+        payload[key] = value
+
+        embed = discord.Embed(title='', description=f'{key} is set to {value}', color=discord.Color.green())
+        await ctx.send(embed=embed)
+
+    @commands.command('sd_print_options', aliases=aliases['sd_print_options'], help='Print options')
+    async def sd_print_options_(self, ctx):
+        payload = await self.get_payload(ctx)
+        await ctx.send(payload)
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(ImageCog(bot))
+    await bot.add_cog(ImageCog())
--- config/img_config.py
+++ config/img_config.py
@@ -1,23 +1,44 @@
+import numpy as np
+
 img_dir='img'
 sd_url='http://127.0.0.1:7860'
 
-txt2image_payload = {
-  'seed': -1,
-  'sampler_name': 'DDIM',
-  'batch_size': 1,
-  'n_iter': 1,
-  'steps': 20,
-  'cfg_scale': 7,
-  'width': 512,
-  'height': 512,
-}
-
-options_payload = {
+txt2img_payload = {
     'sd_model_checkpoint': 'chilloutmix_NiPrunedFp16Fix.safetensors [59ffe2243a]',
+    'seed': -1,
+    'sampler_name': 'DPM++ 2M Karras',
+    'batch_size': 1,
+    'n_iter': 1,
+    'steps': 20,
+    'cfg_scale': 7,
+    'height': 768,
+    'width': 768,
 }
 
 model_map = {
+    'abyssorangemix2': 'abyssorangemix2_Hard.safetensors [e714ee20aa]',
+    'abyssorangemix3': 'abyssorangemix3AOM3_aom3a3.safetensors [eb4099ba9c]',
+    'aoaoko': 'aoaokoPVCStyleModel_pvcAOAOKO.safetensors [cf64507cef]',
     'chilloutmix': 'chilloutmix_NiPrunedFp16Fix.safetensors [59ffe2243a]',
-    'abyssorangemix': 'abyssorangemix3AOM3_aom3a3.safetensors [eb4099ba9c]',
-    
+    'counterfeitv2.5': 'CounterfeitV25_25.safetensors [a074b8864e]',
+    'dreamshaper': 'dreamshaper_4BakedVae.safetensors [7f16bbcd80]',
+    'grapefruit': 'grapefruitHentaiModel_grapefruitv41.safetensors [c590550ea5]',
+    'orangechillmix': 'orangechillmix_v70.safetensors [a92311f07a]',
+    'pastelmix': 'pastelMixStylizedAnime_pastelMixPrunedFP16.safetensors [d01a68ae76]',
+    'perfectworld': 'perfectWorld_v2Baked.safetensors [79e42fb744]',
+    'revanimated': 'revAnimated_v11.safetensors [d725be5d18]',
+}
+
+sfw_models = ['counterfeitv2.5', 'dreamshaper', 'pastelmix', 'revanimated']
+
+nsfw_models = [key for key in model_map.keys() if key not in sfw_models]
+
+txt2img_keys = {'sampler_name', 'steps', 'cfg_scale', 'height', 'width'}
+
+valid_options = {
+    'sampler_name': ['DPM++ SDE Karras', 'DPM++ 2M Karras', 'DDIM', 'Euler a', 'Euler'],
+    'steps': list(range(1, 151)),
+    'cfg_scale': list(np.arange(1, 30.5, 0.5)),
+    'height': [512, 768],
+    'width': [512, 768],
 }
